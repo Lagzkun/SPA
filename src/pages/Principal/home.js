023dcedb68d59b25536db70eb1b2b305c960b003
Lagzkun <lagzlight@.com>
2022-06-28 21:05:23 -0400

    SPA version 0.2

--- src/pages/Principal/home.js
+++ src/pages/Principal/home.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './home.css';
+import 'react-slideshow-image/dist/styles.css'
+import { Slide } from 'react-slideshow-image';
 
 const Home = () => {
+    const sliderInfo = [
+        {"text": 'Slide 1', 'image':'https://images.unsplash.com/photo-1509721434272-b79147e0e708?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80'},
+        {"text": 'Slide 2', 'image': 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80'},
+        {"text": 'Slide 3', 'image': 'https://images.unsplash.com/photo-1506710507565-203b9f24669b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1536&q=80'},
+    ];
     return (
         <div className='container-home'>
-            <h1>Hola soy una SPA</h1>
+            <Slide>
+             {
+                sliderInfo.map((item,index) => (
+                    <div className="each-slide-effect">
+                        <div key={index} style={{ 'backgroundImage': `url(${item.image})` }}>
+                            <span>{item.text}</span>
+                        </div>
+                    </div>
+                ))
+             }  
+        </Slide>
         </div>
     );
 }
--- src/pages/recetas/recetas.js
+++ src/pages/recetas/recetas.js
@@ -1,37 +1,25 @@
 import React from 'react';
 import './blog.css';
 import Card from '../../component/card/card';
-import Arepa from '../../images/arepa.jpg';
+import ItemsFood from '../../Json/recetas.json';
 
 
 const recetas = () => {
-    const titleCard = 'La Receta Secreta de la Arepa'
-    const textCard = 'Hazte millonario vendiendo arepas con langosta'
-    const titleCard2 = 'La Receta de las Arepas Asadas uwu'
-    const titleCard3 = 'La Receta de las Arepas A La Parrilla'
-    const textButtonBlog = 'Leer Receta'
     return (
         <div className='container-blog'>
             <h1>Recetas</h1>
             <div className='card-sector'>
-                <Card
-                    img={Arepa}
-                    text={titleCard}
-                    info={textCard}
-                    button={textButtonBlog}
-                />
-                <Card
-                    img={Arepa}
-                    text={titleCard2}
-                    info={textCard}
-                    button={textButtonBlog}
-                />
-                <Card
-                    img={Arepa}
-                    text={titleCard3}
-                    info={textCard}
-                    button={textButtonBlog}
-                />
+                {
+                    ItemsFood.map((item,index) => (
+                        <Card 
+                        key={index}
+                            img={item.img}
+                            text={item.name}
+                            info={item.info}
+                            button={item.button}
+                        />
+                    )) 
+                }
             </div>
         </div>
     );
